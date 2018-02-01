f5879c8a825383409e5bea35eafc898b6eb4fb2d
Hyukpyo Hong <hyukpyohong@gmail.com>
2018-02-01 17:08:55 -0500

    First Commit

---
+++ Basic/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Basic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Applybutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This Description is ", this.DescriptionText.Text);
+        }
+
+        private void Resetbutton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WeldCheckbox.IsChecked = this.LatheCheckbox.IsChecked = this.AssemblyCheckbox.IsChecked = this.DrillCheckbox.IsChecked = this.PlasmaCheckbox.IsChecked = this.FoldCheckbox.IsChecked = this.LaserCheckbox.IsChecked = this.RollCheckbox.IsChecked = this.PurchaseCheckbox.IsChecked = this.SawCheckbox.IsChecked = false;
+        }
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += ((CheckBox)sender).Content;
+        }
+
+        private void FinishDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteText == null)
+            {
+                return;
+            }
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+            this.NoteText.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FinishDropdown_SelectionChanged(this.FinishDropdown, null);
+        }
+
+        private void SupplierNametext_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassText.Text = this.SupplierNametext.Text;
+        }
+    }
+}
---
+++ WpfTreeView/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// Interaction logic for TreeViews.xaml
+    /// </summary>
+    public partial class TreeViews : Window
+    {
+        #region constructor
+        public TreeViews()
+        {
+            InitializeComponent();
+
+        }
+        #endregion
+
+        #region On Loaded
+        /// <summary>
+        /// When the application first open
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get every logical drive on the machien
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem()
+                {
+                    //Set the header and path
+                    Header = drive,
+                    //And the full path
+                    Tag = drive,
+                };
+
+
+                //Add a dummy item
+                item.Items.Add(null);
+
+                //Listen out for item being expanded
+                item.Expanded += Folder_Expanded;
+
+
+                // Add it to the main tree-view
+                FolderView.Items.Add(item);
+            }
+        }
+        #endregion
+
+
+        #region Folder Expanded
+        /// <summary>
+        /// When a folder is expanded, find the sub folder/file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            #region Initial Checks
+            var item = (TreeViewItem)sender;
+
+            // If the item only contains the dummy data
+            if (item.Items.Count != 1 && item.Items[0] != null)
+                return;
+
+            // Clear dummy data
+            item.Items.Clear();
+
+            // Get full path
+            var fullPath = (string)item.Tag;
+            #endregion
+
+            #region Get Folders
+            // Creat a blank list for directories
+            var directories = new List<string>();
+
+            //Try and get directories form the folder
+            // ignoring any issues doing so
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+            }
+            catch { }
+
+            // For each directory
+            directories.ForEach(directoryPath =>
+            {
+                // Create directory item
+                var subItem = new TreeViewItem()
+                {
+                    // Set header as folder name
+                    Header = GetFileFolderName(directoryPath),
+                    // And tag as full path
+                    Tag = directoryPath
+                };
+
+                // Add dummy item so we can expand folder
+                subItem.Items.Add(null);
+
+                // Handle expanding
+                subItem.Expanded += Folder_Expanded;
+
+                // Add this item to the parent
+                item.Items.Add(subItem);
+
+            });
+            #endregion
+
+            #region Get Files
+            // Creat a blank list for files
+            var files = new List<string>();
+
+            //Try and get directories form the folder
+            // ignoring any issues doing so
+            try
+            {
+                var fs = Directory.GetFiles(fullPath);
+                if (fs.Length > 0)
+                    directories.AddRange(fs);
+            }
+            catch { }
+
+            // For each file...
+            files.ForEach(filePath =>
+            {
+                // Create file item
+                var subItem = new TreeViewItem()
+                {
+                    // Set header as file name
+                    Header = GetFileFolderName(filePath),
+                    // And tag as full path
+                    Tag = filePath
+                };
+
+                // Add this item to the parent
+                item.Items.Add(subItem);
+
+            });
+
+            #endregion
+        }
+        #endregion
+
+
+        #region Helpers
+        /// <summary>
+        /// Find the file or folder name from a full path
+        /// </summary>
+        /// <param name="path"></param>
+        public static string GetFileFolderName(string path)
+        {
+            //If we have no path, return empty
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            // Make all slashed back slashed
+            var nomalizedPath = path.Replace('/', '\\');
+
+            // find the last backslash in the path
+            var lastIndex = nomalizedPath.LastIndexOf('\\');
+
+            // If we don't find a backslash, return the path itself
+            if (lastIndex <= 0)
+                return path;
+
+            //return the name after the last back slash
+            return path.Substring(lastIndex + 1);
+
+        }
+        #endregion
+
+
+    }
+}
